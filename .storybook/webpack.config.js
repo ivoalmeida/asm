b8d3b54640829ab60605f3c50ce960d5a5eb3c73
Ivo Almeida <hyperlegend@hotmail.com>
2019-08-14 14:59:23 +0100

    Added storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const SRC_PATH = path.join(__dirname, '../src/components');
+module.exports = ({ config }) => {
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    include: [SRC_PATH],
+    use: [
+      {
+        loader: require.resolve('babel-loader'),
+      },
+      { loader: require.resolve('react-docgen-typescript-loader') },
+    ],
+  });
+  config.resolve.extensions.push('.ts', '.tsx');
+  return config;
+};
