17b5db02379fe9afb206834befa34830a5967b30
Ivo Almeida <hyperlegend@hotmail.com>
2019-09-10 16:32:19 +0100

    Added stories for remaining icon variants

--- src/components/atoms/icon/index.stories.tsx
+++ src/components/atoms/icon/index.stories.tsx
@@ -7,5 +7,19 @@ import Icon from '.';
 
 storiesOf('Icon', module)
   .addDecorator(withKnobs)
-  .add('Actions Icon', () => <Icon variant={text('variant', 'actions')} />)
-  .add('Trash Icon', () => <Icon variant={text('variant', 'trash')} />);
+  .add('actions', () => <Icon variant={text('variant', 'actions')} />)
+  .add('trash', () => <Icon variant={text('variant', 'trash')} />)
+  .add('changelog', () => <Icon variant={text('variant', 'changelog')} />)
+  .add('cog', () => <Icon variant={text('variant', 'cog')} />)
+  .add('disabled', () => <Icon variant={text('variant', 'disabled')} />)
+  .add('drag', () => <Icon variant={text('variant', 'drag')} />)
+  .add('duplicate', () => <Icon variant={text('variant', 'duplicate')} />)
+  .add('edit', () => <Icon variant={text('variant', 'edit')} />)
+  .add('eye', () => <Icon variant={text('variant', 'eye')} />)
+  .add('filter', () => <Icon variant={text('variant', 'filter')} />)
+  .add('filter2', () => <Icon variant={text('variant', 'filter2')} />)
+  .add('leftArrow', () => <Icon variant={text('variant', 'leftArrow')} />)
+  .add('plus', () => <Icon variant={text('variant', 'plus')} />)
+  .add('rightArrow', () => <Icon variant={text('variant', 'rightArrow')} />)
+  .add('tag', () => <Icon variant={text('variant', 'tag')} />)
+  .add('times', () => <Icon variant={text('variant', 'times')} />);
