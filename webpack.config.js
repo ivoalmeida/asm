cd01ccd7348f3fa0629349efebf487939fd9c3f6
Ivo Almeida <hyperlegend@hotmail.com>
2019-09-10 12:04:30 +0100

    Added clean webpack plugin to clean output directory before a build

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 
@@ -99,5 +100,8 @@ module.exports = {
       chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css',
     }),
     new webpack.WatchIgnorePlugin([/scss\.d\.ts$/]),
+    new CleanWebpackPlugin({
+      verbose: true,
+    }),
   ],
 };
