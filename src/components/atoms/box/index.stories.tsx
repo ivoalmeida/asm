35d2d16b66ab53c874b4eb3afcc5da72a219c8d6
Ivo Almeida <hyperlegend@hotmail.com>
2019-09-11 15:15:26 +0100

    Added Box more stories. Fixed styles

--- src/components/atoms/box/index.stories.tsx
+++ src/components/atoms/box/index.stories.tsx
@@ -4,23 +4,50 @@ import { text, withKnobs } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 import Box from '.';
+import Button from '../button';
 
 storiesOf('Box', module)
   .addDecorator(withKnobs)
-  .add('Box with text', () => (
+  .add('Box row', () => (
     <Box>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ullam quos voluptas molestias
-      corporis aperiam a dignissimos aut natus ducimus iste voluptatibus fuga saepe, nesciunt
-      distinctio, sed placeat dolores. Autem.
+      <div style={{ backgroundColor: `#98D2EB` }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ullam quos voluptas molestias
+        corporis aperiam a dignissimos aut natus ducimus iste voluptatibus fuga saepe, nesciunt
+        distinctio, sed placeat dolores. Autem.
+      </div>
+      <div style={{ backgroundColor: `#D8315B` }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ullam quos voluptas molestias
+        corporis aperiam a dignissimos aut natus ducimus iste voluptatibus fuga saepe, nesciunt
+        distinctio, sed placeat dolores. Autem.
+      </div>
     </Box>
   ))
-  .add('Box with text and title', () => (
-    <Box>
-      <h1>Ducimus Iste Voluptatibus</h1>
-      <p>
+  .add('Box column', () => (
+    <Box direction="column">
+      <div style={{ backgroundColor: `#78FECF` }}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ullam quos voluptas molestias
-        corporis aperiam a dignissimos aut natus fuga saepe, nesciunt distinctio, sed placeat
-        dolores. Autem.
-      </p>
+        corporis aperiam a dignissimos aut natus ducimus iste voluptatibus fuga saepe, nesciunt
+        distinctio, sed placeat dolores. Autem.
+      </div>
+      <div style={{ backgroundColor: `#7F96FF` }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ullam quos voluptas molestias
+        corporis aperiam a dignissimos aut natus ducimus iste voluptatibus fuga saepe, nesciunt
+        distinctio, sed placeat dolores. Autem.
+      </div>
+    </Box>
+  ))
+  .add('Box buttons', () => (
+    <Box
+      direction="column"
+      w={500}
+      h={250}
+      styles={{ border: `1px solid #f3f3f3`, justifyContent: `flex-end` }}
+    >
+      <Button variant="primary" size="large">
+        Login
+      </Button>
+      <Button variant="default" size="large">
+        Resend verification code
+      </Button>
     </Box>
   ));
--- src/components/atoms/box/index.tsx
+++ src/components/atoms/box/index.tsx
@@ -3,9 +3,20 @@ import * as styles from './styles.scss';
 import classNames from 'classnames';
 
 interface IBoxProps {
+  direction?: string;
+  w?: number;
+  h?: number;
+  styles?: React.CSSProperties;
   children?: any;
 }
 
-export const Box = (props: IBoxProps) => <div className={styles.box}>{props.children}</div>;
+export const Box = (props: IBoxProps) => (
+  <div
+    className={classNames(styles.box, { [styles.column]: props.direction === 'column' })}
+    style={Object.assign({}, { width: `${props.w}px`, height: `${props.h}px` }, props.styles)}
+  >
+    {props.children}
+  </div>
+);
 
 export default Box;
