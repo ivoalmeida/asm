b44a24279c3d52222a75f8fcbb6a6f9e8983664a
Ivo Almeida <hyperlegend@hotmail.com>
2019-09-13 17:43:20 +0100

    Added logo font face and files. Changed Logo component and added styles

--- src/components/atoms/logo/index.tsx
+++ src/components/atoms/logo/index.tsx
@@ -1,58 +1,12 @@
 import * as React from 'react';
+import cn from 'classnames';
 
-interface IProps {
-  width?: number;
-  height?: number;
-  fillColor?: string;
-  className?: string;
-}
+import * as styles from './styles.scss';
 
-const Logo: React.SFC<IProps> = ({ width = 158, height = 82, ...props }) => {
-  return (
-    <svg
-      className={props.className}
-      xmlns="http://www.w3.org/2000/svg"
-      width={width}
-      height={height}
-      viewBox={`0 0 ${width} ${height}`}
-    >
-      <g fill="none" fill-rule="evenodd">
-        <rect width={width} height={height} fill="#C6161D" />
-        <text
-          fill="#FFF"
-          font-family="Arvo-Bold, Arvo"
-          font-size="28"
-          font-weight="bold"
-          letter-spacing="-.933"
-        >
-          <tspan x="24" y="59">
-            venatus
-          </tspan>
-        </text>
-      </g>
-    </svg>
-  );
-};
+const Logo = () => <div className={cn(styles.logo, styles.text)}>venatus</div>;
 
-export const LogoMastHead: React.SFC<IProps> = props => {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="108" height="100" viewBox="0 0 108 100">
-      <g fill="none" fill-rule="evenodd">
-        <rect width="108" height="100" fill="#C6161D" />
-        <text
-          fill="#FFF"
-          font-family="Arvo-Bold, Arvo"
-          font-size="28"
-          font-weight="bold"
-          letter-spacing="-.933"
-        >
-          <tspan x="45" y="60">
-            v
-          </tspan>
-        </text>
-      </g>
-    </svg>
-  );
-};
+export const LogoMastHead = () => (
+  <div className={cn(styles.logo, styles.text, styles.masthead)}>v</div>
+);
 
 export default Logo;
