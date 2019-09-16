e87120b9aeb44b5228a9c91a9b61d36a47bbe180
Ivo Almeida <hyperlegend@hotmail.com>
2019-09-16 11:49:10 +0100

    Added jest and enzyme

--- src/components/organisms/forgotPassword/index.test.tsx
+++ src/components/organisms/forgotPassword/index.test.tsx
@@ -1,2 +1,17 @@
 import * as React from 'react';
-import * as styles from './styles.scss';
+import { shallow } from 'enzyme';
+
+import ForgotPassword from '.';
+
+describe('ForgetPassword tests', () => {
+  const wrapper = shallow(<ForgotPassword />);
+
+  it('should render the component', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders the heading', () => {
+    const result = wrapper.contains('Forgot Password');
+    expect(result).toBeTruthy();
+  });
+});
