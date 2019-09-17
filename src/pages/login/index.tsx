3a55b75c75a521d3b1e1c37221ce3b801106261d
Ivo Almeida <hyperlegend@hotmail.com>
2019-09-17 17:39:35 +0100

    Added graphql dependencies. Implemented login container

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,18 +1,44 @@
 import * as React from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import Box from '../../components/atoms/box';
 import Login from '../../components/organisms/login';
+
+const LOGIN_MUTATION = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      email
+      password
+    }
+  }
+`;
 
 export interface ILoginProps {
   children?: any;
 }
 
-export default class LoginPage extends React.Component<ILoginProps> {
+export interface IState {
+  email: string;
+  password: string;
+}
+
+export default class LoginPage extends React.Component<ILoginProps, IState> {
+  constructor(props: ILoginProps) {
+    super(props);
+  }
+
   public render() {
     return (
       <Box direction="column" styles={{ justifyContent: 'center', alignItems: 'center' }}>
-        <Login />
+        <Mutation mutation={LOGIN_MUTATION} variables={this.state}>
+          {(login: any) => <Login onClick={login} />}
+        </Mutation>
       </Box>
     );
   }
+  private doLogin = (email: string, password: string) => {
+    this.setState({ email, password });
+    // call graphql here
+  };
 }
