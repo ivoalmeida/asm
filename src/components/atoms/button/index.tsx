5f8cac6b06d9fa71f6fb11ca054aa28c72ebddd9
Ivo Almeida <hyperlegend@hotmail.com>
2019-09-18 12:20:49 +0100

    Refactored button component to include native props

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -5,40 +5,48 @@ import classNames from 'classnames';
 export interface IButtonProps {
   variant: string;
   size?: string;
-  children?: any;
-  className?: string;
-  styles?: React.CSSProperties;
   href?: string;
-  onClick?: (ev?: React.SyntheticEvent | Event) => void;
 }
 
-const getButtonStyles = (props: IButtonProps) => {
+const getButtonStyles = (variant: string, size: string, className?: string) => {
   return classNames(
     styles.button,
-    { [styles.primary]: props.variant === 'primary' },
-    { [styles.secondary]: props.variant === 'secondary' },
-    { [styles.danger]: props.variant === 'danger' },
-    { [styles.success]: props.variant === 'success' },
-    { [styles.navItem]: props.variant === 'nav-item' },
-    { [styles.small]: props.size === 'small' },
-    { [styles.medium]: props.size === 'medium' },
-    { [styles.large]: props.size === 'large' },
-    { [styles.xlarge]: props.size === 'xlarge' },
-    props.className,
+    { [styles.primary]: variant === 'primary' },
+    { [styles.secondary]: variant === 'secondary' },
+    { [styles.danger]: variant === 'danger' },
+    { [styles.success]: variant === 'success' },
+    { [styles.navItem]: variant === 'nav-item' },
+    { [styles.small]: size === 'small' },
+    { [styles.medium]: size === 'medium' },
+    { [styles.large]: size === 'large' },
+    { [styles.xlarge]: size === 'xlarge' },
+    className,
   );
 };
 
-export const Button = (props: IButtonProps) => {
+export const Button: React.SFC<IButtonProps & React.ButtonHTMLAttributes<HTMLButtonElement>> = ({
+  variant,
+  size,
+  className,
+  children,
+  ...props
+}) => {
   return (
-    <button style={props.styles} className={getButtonStyles(props)}>
-      {props.children}
+    <button {...props} className={getButtonStyles(variant, size, className)}>
+      {children}
     </button>
   );
 };
 
-export const LinkButton = (props: IButtonProps) => (
-  <a href={props.href} onClick={props.onClick} style={props.styles} className={styles.navItem}>
-    {props.children}
+export const LinkButton: React.SFC<IButtonProps & React.LinkHTMLAttributes<HTMLAnchorElement>> = ({
+  variant,
+  size,
+  className,
+  children,
+  ...props
+}) => (
+  <a {...props} className={classNames(styles.navItem, className)}>
+    {children}
   </a>
 );
 
