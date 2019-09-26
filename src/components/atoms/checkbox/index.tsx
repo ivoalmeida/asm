bc8baadc5b536a112dd319573dd1d3d30dd2b6af
Ivo Almeida <hyperlegend@hotmail.com>
2019-09-26 16:25:20 +0100

    Added multi seleciton css class and prop to style checkbox for multiple selections

--- src/components/atoms/checkbox/index.tsx
+++ src/components/atoms/checkbox/index.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
+import cn from 'classnames';
 
 import * as styles from './styles.scss';
 
-const Checkbox: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = props => {
+interface IProps {
+  isMultiSelection?: boolean;
+}
+
+const Checkbox: React.FC<IProps & React.InputHTMLAttributes<HTMLInputElement>> = ({
+  isMultiSelection,
+  ...props
+}) => {
   return (
     <label className={styles.checkbox}>
       <input type="checkbox" {...props} />
-      <span className={styles.checkmark} />
+      <span className={cn(styles.checkmark, { [styles.multiSelection]: isMultiSelection })} />
     </label>
   );
 };
