a388ddbddd24cafcda5e6adecbd3e88551c78f88
Ivo Almeida <hyperlegend@hotmail.com>
2019-09-27 12:12:54 +0100

    Added nav link buttons to masthead

--- src/components/organisms/masthead/index.tsx
+++ src/components/organisms/masthead/index.tsx
@@ -6,24 +6,22 @@ import Box from '../../atoms/box';
 import { LogoMastHead } from '../../atoms/logo';
 import { LinkButton } from '../../atoms/button';
 import SearchBox from '../../molecules/search-box';
+import NavBar from '../navbar';
+import NavLinkButton from '../../atoms/nav-link-button';
 
 export const Masthead = () => {
   return (
     <div className={styles.masthead}>
-      <LogoMastHead />
+      <Box padding={'0 25px 0 0'}>
+        <LogoMastHead />
+      </Box>
       <Box>
-        <LinkButton variant="nav-item" href="/accounts">
-          Accounts
-        </LinkButton>
-        <LinkButton variant="nav-item" href="/users">
-          Users
-        </LinkButton>
-        <LinkButton variant="nav-item" href="/networks">
-          Networks
-        </LinkButton>
-        <LinkButton variant="nav-item" href="/finance">
-          Finance
-        </LinkButton>
+        <NavBar>
+          <NavLinkButton to="/accounts">Accounts</NavLinkButton>
+          <NavLinkButton to="/users">Users</NavLinkButton>
+          <NavLinkButton to="/networks">Networks</NavLinkButton>
+          <NavLinkButton to="/finance">Finance</NavLinkButton>
+        </NavBar>
       </Box>
       <Box>
         <SearchBox />
