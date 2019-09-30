7a18eda6f601f195aff9953bfd5391417f4d5b85
Ivo Almeida <hyperlegend@hotmail.com>
2019-09-30 15:44:00 +0100

    Added react router dom  Browserrouter parent component to fix error on storybook

--- src/pages/users/index.stories.tsx
+++ src/pages/users/index.stories.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import MockServer from '../../containers/mock-server';
 import UsersPage from '.';
 
 storiesOf('Users Page', module).add('Users data grid', () => (
   <MockServer>
-    <UsersPage />
+    <Router>
+      <UsersPage />
+    </Router>
   </MockServer>
 ));
