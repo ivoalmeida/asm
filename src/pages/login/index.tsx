ae67f0748dee2b71b7865de6c739d3b1125dde83
Ivo Almeida <hyperlegend@hotmail.com>
2019-10-03 14:33:36 +0100

    Converted login page component from class to function component and use apollo mutation hooks. Replaced individual form components by the login presentational component

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
-import { Mutation } from 'react-apollo';
+import { useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
 import Box from '../../components/atoms/box';
-import Button from '../../components/atoms/button';
-import Input from '../../components/atoms/input';
-import Label from '../../components/atoms/label';
-import Logo from '../../components/atoms/logo';
 import Login from '../../components/organisms/login';
+import { Redirect } from 'react-router';
 
 const LOGIN_MUTATION = gql`
   mutation login($email: String!, $password: String!) {
@@ -18,79 +15,26 @@ const LOGIN_MUTATION = gql`
   }
 `;
 
-export interface ILoginProps {
-  children?: any;
-  history?: any;
-}
+const LoginPage: React.FC = () => {
+  const [email, setEmail] = React.useState();
+  const [password, setPwd] = React.useState();
+  const [login, { error, data }] = useMutation(LOGIN_MUTATION, { variables: { email, password } });
 
-export interface IState {
-  email?: string;
-  password?: string;
-  token?: string;
-}
-
-export default class LoginPage extends React.Component<ILoginProps, IState> {
-  constructor(props: ILoginProps) {
-    super(props);
-    this.state = {
-      email: undefined,
-      password: undefined,
-      token: undefined,
-    };
+  if (data) {
+    return <Redirect to="/users" />;
   }
-
-  public render() {
-    const inputStyles: React.CSSProperties = {
-      width: `100%`,
-      marginBottom: `10px`,
-    };
-    const { email, password } = this.state;
-    return (
-      <Box justifyContent="center">
-        <Box direction="column" padding="10vh 5px" width="400px">
-          <Box justifyContent="center">
-            <Logo />
-          </Box>
-          <h1 style={{ padding: `40px 0` }}>Login</h1>
-          <Label>E-mail Address</Label>
-          <Input
-            placeholder="email"
-            style={inputStyles}
-            value={email}
-            onChange={e => this.setState({ email: e.target.value })}
-          />
-          <Label>Password</Label>
-          <Input
-            placeholder="password"
-            type="password"
-            style={inputStyles}
-            value={password}
-            onChange={e => this.setState({ password: e.target.value })}
-          />
-
-          <Mutation
-            mutation={LOGIN_MUTATION}
-            variables={this.state}
-            onCompleted={data => this.confirm(data)}
-          >
-            {(login: any) => (
-              <Button variant="primary" size="large" onClick={login}>
-                Login
-              </Button>
-            )}
-          </Mutation>
-        </Box>
-      </Box>
-    );
+  if (error) {
+    return <div>{`An error has occured: ${error.message}`}</div>;
   }
+  return (
+    <Box justifyContent="center">
+      <Login
+        onEmailChange={setEmail}
+        onPwdChange={setPwd}
+        onSubmit={() => email && password && login()}
+      />
+    </Box>
+  );
+};
 
-  private confirm = data => {
-    const { token } = this.state.token ? data.login : data.signup;
-    this.saveUserData(token);
-    this.props.history.push(`/users`);
-  };
-
-  private saveUserData = token => {
-    document.cookie = `AUTH_TOKEN=${token}`;
-  };
-}
+export default LoginPage;
