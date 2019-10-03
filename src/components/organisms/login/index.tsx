f81f3bec330559cd54e33e00a9a8d67e0d349468
Ivo Almeida <hyperlegend@hotmail.com>
2019-10-03 14:31:14 +0100

    Added field components and added change event and submit function props. Fixed forgot link position style

--- src/components/organisms/login/index.tsx
+++ src/components/organisms/login/index.tsx
@@ -1,40 +1,42 @@
 import * as React from 'react';
-import classNames from 'classnames';
 
-import Box from '../../atoms/box';
-import Button from '../../atoms/button';
-import Input from '../../atoms/input';
-import Label from '../../atoms/label';
-import Logo from '../../atoms/logo';
 import * as styles from './styles.scss';
+import Button from '../../atoms/button';
+import Logo from '../../atoms/logo';
+import Field from '../../molecules/field';
 
 interface IProps {
-  children?: any;
-  onClick?: (email: string, pwd: string) => void;
+  onEmailChange?: (value: string) => void;
+  onPwdChange?: (value: string) => void;
+  onSubmit: () => void;
 }
 
-const inputStyles: React.CSSProperties = {
-  width: `100%`,
-  marginBottom: `10px`,
-};
-
-const Login: React.FC<IProps> = props => {
+const Login: React.FC<IProps> = ({ onEmailChange, onPwdChange, onSubmit }) => {
   return (
     <div className={styles.login}>
-      <Box styles={{ justifyContent: 'center' }}>
+      <span className={styles.logo}>
         <Logo />
-      </Box>
+      </span>
       <h1 style={{ padding: `40px 0` }}>Login</h1>
-      <Label>E-mail Address</Label>
-      <Input placeholder="email" style={inputStyles} />
-      <Label>Password</Label>
+      <Field
+        name="email"
+        label="E-mail Address"
+        placeholder="email"
+        onChange={e => onEmailChange(e.target.value)}
+      />
+
       <div className={styles.pwdLink}>
-        <Input placeholder="password" type="password" style={inputStyles} />
+        <Field
+          name="pwd"
+          label="Password"
+          type="password"
+          onChange={e => onPwdChange(e.target.value)}
+        />
         <a className={styles.forgot} href="#">
           Forgot?
         </a>
       </div>
-      <Button variant="primary" size="large">
+      <Button variant="primary" size="large" onClick={onSubmit}>
         Login
       </Button>
     </div>
