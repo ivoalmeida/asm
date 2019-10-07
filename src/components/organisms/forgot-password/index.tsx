16821fa366865467e9e52bb52f3ea3b862bd627f
Ivo Almeida <hyperlegend@hotmail.com>
2019-10-07 10:29:24 +0100

    Refactored forgot pwd component. Added props and added field component dependency

--- src/components/organisms/forgot-password/index.tsx
+++ src/components/organisms/forgot-password/index.tsx
@@ -2,31 +2,33 @@ import * as React from 'react';
 
 import Box from '../../atoms/box';
 import Button from '../../atoms/button';
-import Input from '../../atoms/input';
-import Label from '../../atoms/label';
 import Logo from '../../atoms/logo';
+import Field from '../../molecules/field';
 import * as styles from './styles.scss';
 
 interface IProps {
-  children?: any;
-  onClick?: (email: string, pwd: string) => void;
+  email: string;
+  errorMessage?: string;
+  onClick: () => void;
+  onChange: (e) => void;
 }
 
-const inputStyles: React.CSSProperties = {
-  width: `100%`,
-  marginBottom: `10px`,
-};
-
-const ForgotPassword: React.FC<IProps> = props => {
+const ForgotPassword: React.FC<IProps> = ({ email, errorMessage, onClick, onChange }) => {
   return (
     <div className={styles.forgot}>
       <Box justifyContent="center">
         <Logo />
       </Box>
       <h1 style={{ padding: `40px 0` }}>Forgot Password</h1>
-      <Label>E-mail Address</Label>
-      <Input placeholder="email" style={inputStyles} />
-      <Button variant="primary" size="large">
+      <Field
+        name="email"
+        label="E-mail Address"
+        value={email}
+        placeholder="name@email.com"
+        onChange={onChange}
+        errorMessage={errorMessage}
+      />
+      <Button variant="primary" size="large" onClick={onClick}>
         Send Reset Link
       </Button>
     </div>
