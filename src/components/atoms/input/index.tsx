fba3ab2c9b53bde7b4104fc82ad887252d6a7590
Ivo Almeida <hyperlegend@hotmail.com>
2019-10-22 13:29:59 +0100

    Added textarea component and styles

--- src/components/atoms/input/index.tsx
+++ src/components/atoms/input/index.tsx
@@ -7,6 +7,11 @@ interface IInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   invalid?: boolean;
 }
 
+interface ITextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  errorMessage?: string;
+  invalid?: boolean;
+}
+
 export const Input: React.SFC<IInputProps> = ({ errorMessage, invalid, className, ...props }) => {
   const displayErrorMessage = errorMessage && errorMessage.length > 0;
   const isError: boolean = invalid || displayErrorMessage;
@@ -14,6 +19,32 @@ export const Input: React.SFC<IInputProps> = ({ errorMessage, invalid, className
   return (
     <span className={cn({ [styles.errorWarning]: isError })}>
       <input
+        className={cn(
+          styles.inputText,
+          {
+            [styles.error]: isError,
+          },
+          className,
+        )}
+        {...props}
+      />
+      {displayErrorMessage && <span className={styles.errorMessage}>{errorMessage}</span>}
+    </span>
+  );
+};
+
+export const Textarea: React.FC<ITextareaProps> = ({
+  errorMessage,
+  invalid,
+  className,
+  ...props
+}) => {
+  const displayErrorMessage = errorMessage && errorMessage.length > 0;
+  const isError: boolean = invalid || displayErrorMessage;
+
+  return (
+    <span className={cn({ [styles.errorWarning]: isError })}>
+      <textarea
         className={cn(
           styles.inputText,
           {
