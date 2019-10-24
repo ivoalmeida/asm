62a297b0cd0abac4ffced9126f348e8eba98b321
Ivo Almeida <hyperlegend@hotmail.com>
2019-10-24 16:14:30 +0100

    Added state to control form elements

--- src/pages/accounts/edit-account.tsx
+++ src/pages/accounts/edit-account.tsx
@@ -10,7 +10,36 @@ interface IProps {
   onCancel: () => void;
 }
 
+interface IState {
+  accountname?: string;
+  companyname?: string;
+  addressline1?: string;
+  addressline2?: string;
+  postcode?: string;
+  accountmanager?: string;
+  country?: number;
+  state?: number;
+  city?: number;
+}
+
+const initialState: IState = {
+  accountname: '',
+  companyname: '',
+  addressline1: '',
+  addressline2: '',
+  postcode: '',
+  accountmanager: '',
+  country: -1,
+  state: -1,
+  city: -1,
+};
+
 const EditAccount: React.FC<IProps> = ({ isOpen, onCancel }) => {
+  const [state, setState] = React.useState<IState>(initialState);
+  const handleChange = ev => {
+    const { name, value } = ev.target;
+    setState({ ...state, [name]: value });
+  };
   return (
     <SidePanel isVisible={isOpen} onClose={onCancel}>
       <Form
@@ -26,14 +55,35 @@ const EditAccount: React.FC<IProps> = ({ isOpen, onCancel }) => {
           </>
         }
       >
-        <Field name="account_name" label="Account Name" />
-        <Field name="company_name" label="Company Name" />
-        <Field name="address_line1" label="Address Line 1" />
-        <Field name="address_line2" label="Address Line 2" />
+        <Field
+          name="accountname"
+          label="Account Name"
+          value={state.accountname}
+          onChange={handleChange}
+        />
+        <Field
+          name="companyname"
+          label="Company Name"
+          value={state.companyname}
+          onChange={handleChange}
+        />
+        <Field
+          name="addressline1"
+          label="Address Line 1"
+          value={state.addressline1}
+          onChange={handleChange}
+        />
+        <Field
+          name="addressline2"
+          label="Address Line 2"
+          value={state.addressline2}
+          onChange={handleChange}
+        />
         <Field
           name="country"
           label="Country"
           type="dropdown"
+          onChange={handleChange}
           options={[
             { label: 'United Kingdom', value: 1 },
             { label: 'France', value: 2 },
@@ -44,6 +94,7 @@ const EditAccount: React.FC<IProps> = ({ isOpen, onCancel }) => {
           name="state"
           label="state"
           type="dropdown"
+          onChange={handleChange}
           options={[
             { label: 'Lisbon', value: 1 },
             { label: 'Madeira', value: 2 },
@@ -52,23 +103,25 @@ const EditAccount: React.FC<IProps> = ({ isOpen, onCancel }) => {
         />
         <Field
           name="city"
-          label="city"
+          label="City"
           type="dropdown"
+          onChange={handleChange}
           options={[
             { label: 'Vila Nova de Gaia', value: 1 },
             { label: 'Porto', value: 2 },
             { label: 'Matosinhos', value: 3, default: true },
           ]}
         />
-        <Field name="post_code" label="Post Code" />
+        <Field name="postcode" label="Post Code" value={state.postcode} onChange={handleChange} />
         <hr style={{ gridColumn: '1 / span 2' }} />
         <span style={{ gridColumn: '1 / span 2' }}>
           <Field name="comments" label="Comments" type="textarea" style={{ height: '180px' }} />
         </span>
         <Field
-          name="account_manager"
+          name="accountmanager"
           label="AccountManager"
           type="dropdown"
+          onChange={handleChange}
           options={[
             { label: 'Bob', value: 1 },
             { label: 'Beth', value: 2 },
