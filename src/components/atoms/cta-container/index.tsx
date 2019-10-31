2248b569e1cc677aed93aeb80f5e1e92a190f1b1
Ivo Almeida <hyperlegend@hotmail.com>
2019-10-31 16:53:49 +0000

    Added align prop to cta container in order to control where children are rendered

--- src/components/atoms/cta-container/index.tsx
+++ src/components/atoms/cta-container/index.tsx
@@ -1,13 +1,25 @@
 import * as React from 'react';
+import cn from 'classnames';
 
 import * as styles from './styles.scss';
 
 interface IProps {
+  align: string | 'left|right';
   children: any;
 }
 
-const CtaContainer: React.FC<IProps> = ({ children }) => {
-  return <div className={styles.cta}>{children}</div>;
+const CtaContainer: React.FC<IProps> = ({ children, align }) => {
+  return (
+    <div
+      className={cn(
+        styles.cta,
+        { [styles.right]: align === 'right' },
+        { [styles.left]: align === 'left' },
+      )}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default CtaContainer;
