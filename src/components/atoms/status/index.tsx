dd07a2479111a4e9ee526ae4d77a5fe896092693
Ivo Almeida <hyperlegend@hotmail.com>
2019-11-01 16:41:59 +0000

    Implemented component

---
+++ src/components/atoms/status/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import cn from 'classnames';
+
+import * as styles from './styles.scss';
+
+interface IProps {
+  active: boolean;
+}
+
+const Status: React.FC<IProps> = ({ active }) => {
+  return <div className={cn(styles.status, { [styles.active]: active })} />;
+};
+
+export default Status;
