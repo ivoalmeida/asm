7408f495efde6ccb2b8330533898906bcc4c27e4
Ivo Almeida <hyperlegend@hotmail.com>
2019-11-01 10:53:16 +0000

    Added status component

---
+++ src/components/atoms/status/index.test.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+// import Status from '.';
+
+describe('Status tests', () => {
+  // const wrapper = shallow(<Status />);
+
+  it('should render the component', () => {
+    expect(1 === 1).toBe(true);
+  });
+});
