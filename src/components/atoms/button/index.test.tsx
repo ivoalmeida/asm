032e47d5b7131a912b07bec7109479c81541d32e
Ivo Almeida <hyperlegend@hotmail.com>
2019-11-05 16:05:48 +0000

    Added more tests for button component Added jest-css-modules-transform package in order to tranform scss modules in jest tests Switched on synthetic imports in ts config to work around issue with css modules and jest

--- src/components/atoms/button/index.test.tsx
+++ src/components/atoms/button/index.test.tsx
@@ -1,13 +1,99 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
+
 import Button, { IButtonProps } from '.';
 
 describe('Button tests', () => {
   const wrap = (props: IButtonProps = { variant: 'primary', children: '' }) =>
-    shallow(<Button {...props} />);
+    mount(<Button {...props} />);
 
   it('renders children when passed in', () => {
     const wrapper = wrap({ children: 'test', variant: 'danger' });
     expect(wrapper.contains('test')).toBe(true);
+    wrapper.unmount();
+  });
+
+  it('should render a default button when no variant prop is passed', () => {
+    const wrapper = wrap();
+    expect(wrapper.find('.button')).toHaveLength(1);
+    wrapper.unmount();
+  });
+
+  it('should render a default button', () => {
+    const variant = 'default';
+    const wrapper = wrap({ variant });
+    expect(wrapper.props().variant).toEqual(variant);
+    wrapper.unmount();
+  });
+
+  it('should render a primary button', () => {
+    const variant = 'primary';
+    const wrapper = wrap({ variant });
+    expect(wrapper.props().variant).toEqual(variant);
+    expect(wrapper.find(`.${variant}`)).toHaveLength(1);
+    wrapper.unmount();
+  });
+
+  it('should render a danger button', () => {
+    const variant = 'danger';
+    const wrapper = wrap({ variant });
+    expect(wrapper.props().variant).toEqual(variant);
+    expect(wrapper.find(`.${variant}`)).toHaveLength(1);
+    wrapper.unmount();
+  });
+
+  it('should render a secondary button', () => {
+    const variant = 'secondary';
+    const wrapper = wrap({ variant });
+    expect(wrapper.props().variant).toEqual(variant);
+    expect(wrapper.find(`.${variant}`)).toHaveLength(1);
+    wrapper.unmount();
+  });
+
+  it('should render a success button', () => {
+    const variant = 'success';
+    const wrapper = wrap({ variant });
+    expect(wrapper.props().variant).toEqual(variant);
+    expect(wrapper.find(`.${variant}`)).toHaveLength(1);
+    wrapper.unmount();
+  });
+
+  it('should render a small button', () => {
+    const size = 'small';
+    const wrapper = wrap({ size });
+    expect(wrapper.props().size).toEqual(size);
+    expect(wrapper.find(`.${size}`)).toHaveLength(1);
+    wrapper.unmount();
+  });
+
+  it('should render a medium button', () => {
+    const size = 'medium';
+    const wrapper = wrap({ size });
+    expect(wrapper.props().size).toEqual(size);
+    expect(wrapper.find(`.${size}`)).toHaveLength(1);
+    wrapper.unmount();
+  });
+
+  it('should render a large button', () => {
+    const size = 'large';
+    const wrapper = wrap({ size });
+    expect(wrapper.props().size).toEqual(size);
+    expect(wrapper.find(`.${size}`)).toHaveLength(1);
+    wrapper.unmount();
+  });
+
+  it('should render a xlarge button', () => {
+    const size = 'xlarge';
+    const wrapper = wrap({ size });
+    expect(wrapper.props().size).toEqual(size);
+    expect(wrapper.find(`.${size}`)).toHaveLength(1);
+  });
+
+  it('should should be clickable', () => {
+    const clickFn = jest.fn();
+    const wrapper = wrap({ onClick: clickFn });
+    wrapper.find('button').simulate('click');
+    expect(clickFn).toHaveBeenCalled();
+    wrapper.unmount();
   });
 });
