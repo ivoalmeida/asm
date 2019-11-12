64733626df8c1cb532e80af988b7572591a5593a
Ivo Almeida <hyperlegend@hotmail.com>
2019-11-12 09:48:38 +0000

    Added unit test for block component

--- src/components/atoms/block/index.test.tsx
+++ src/components/atoms/block/index.test.tsx
@@ -1,12 +1,25 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
+
 import Block from '.';
 
 describe('Block tests', () => {
   const wrap = (props = {}) => shallow(<Block {...props} />);
 
+  it('renders component', () => {
+    const wrapper = wrap();
+    expect(wrapper).toMatchSnapshot();
+  });
+
   it('renders children when passed in', () => {
-    const wrapper = wrap({ children: 'test' });
-    expect(wrapper.contains('test')).toBe(true);
+    const children = (
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi iure voluptatem facere a,
+        optio eaque nisi ipsum soluta, ea, corrupti eos deleniti nesciunt harum libero! Minus
+        facilis vitae maiores libero?
+      </p>
+    );
+    const wrapper = wrap({ children });
+    expect(wrapper.contains(children)).toBe(true);
   });
 });
