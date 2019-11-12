2883b47e97e65e15313373132da25f61c12bc79b
Ivo Almeida <hyperlegend@hotmail.com>
2019-11-12 11:25:35 +0000

    Added unit tests to dropdown

--- src/components/atoms/dropdown/index.test.tsx
+++ src/components/atoms/dropdown/index.test.tsx
@@ -1,21 +1,39 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
-import Dropdown from '.';
+import { mount } from 'enzyme';
+import Dropdown, { IDropDownProps, ISelectItem } from '.';
 
 describe('Dropdown tests', () => {
-  const wrapper = shallow(
-    <Dropdown
-      options={[
-        { label: 'Option 1', value: 22 },
-        { label: 'Option 2', value: 33 },
-        { label: 'Option 3', value: 44 },
-        { label: 'Option 4', value: 55 },
-        { label: 'Option 5', value: 66 },
-      ]}
-    />,
-  );
+  const wrap = (props: IDropDownProps) => mount(<Dropdown {...props} />);
+
+  const options = [
+    { label: 'Option 1', value: 22 },
+    { label: 'Option 2', value: 33 },
+    { label: 'Option 3', value: 44 },
+    { label: 'Option 4', value: 55 },
+    { label: 'Option 5', value: 66 },
+  ];
 
   it('should render the component', () => {
-    expect(wrapper).toMatchSnapshot();
+    expect(wrap({ options })).toMatchSnapshot();
+  });
+
+  it('should render dropdown with label', () => {
+    const label = 'something';
+    const wrapper = wrap({ options, label });
+    expect(wrapper.props().label).toEqual(label);
+    expect(wrapper.find('.label')).toHaveLength(1);
+    wrapper.unmount();
+  });
+
+  it('should render dropdown with default option', () => {
+    const defaultValue = 99;
+    const optionsWithDefault = [
+      ...options,
+      { label: 'Select me', value: defaultValue, default: true },
+    ];
+    const wrapper = wrap({ options: optionsWithDefault });
+    expect(wrapper.props().options).toEqual(optionsWithDefault);
+    expect(wrapper.find('select').props().value).toEqual(defaultValue);
+    wrapper.unmount();
   });
 });
