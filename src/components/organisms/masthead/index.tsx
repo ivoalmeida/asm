98e709f0d2a3a513814cae98ab023bb178646a75
Ivo Almeida <hyperlegend@hotmail.com>
2019-11-13 14:52:17 +0000

    Added profile component to masthead

--- src/components/organisms/masthead/index.tsx
+++ src/components/organisms/masthead/index.tsx
@@ -8,25 +8,29 @@ import { LinkButton } from '../../atoms/button';
 import SearchBox from '../../molecules/search-box';
 import NavBar from '../navbar';
 import NavLinkButton from '../../atoms/nav-link-button';
+import Profile from '../profile';
 
 export const Masthead = () => {
   return (
-    <div className={styles.masthead}>
-      <Box padding={'0 25px 0 0'}>
-        <LogoMastHead />
+    <Box direction="row" justifyContent="space-between" alignItems="center">
+      <Box direction="row" justifyContent="flex-start" alignItems="center" width="100%">
+        <Box padding={'0 25px 0 0'}>
+          <LogoMastHead />
+        </Box>
+        <Box>
+          <NavBar>
+            <NavLinkButton to="/accounts">Accounts</NavLinkButton>
+            <NavLinkButton to="/users">Users</NavLinkButton>
+            <NavLinkButton to="/networks">Networks</NavLinkButton>
+            <NavLinkButton to="/finance">Finance</NavLinkButton>
+          </NavBar>
+        </Box>
+        <Box margin="0 0 0 80px">
+          <SearchBox />
+        </Box>
       </Box>
-      <Box>
-        <NavBar>
-          <NavLinkButton to="/accounts">Accounts</NavLinkButton>
-          <NavLinkButton to="/users">Users</NavLinkButton>
-          <NavLinkButton to="/networks">Networks</NavLinkButton>
-          <NavLinkButton to="/finance">Finance</NavLinkButton>
-        </NavBar>
-      </Box>
-      <Box>
-        <SearchBox />
-      </Box>
-    </div>
+      <Profile user={{ name: 'John', surname: 'Doe' }} />
+    </Box>
   );
 };
 
