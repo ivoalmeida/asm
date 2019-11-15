420a9d6390eb13b4472d8fdb1809d023ff1333a7
Ivo Almeida <hyperlegend@hotmail.com>
2019-11-15 15:18:21 +0000

    Added unit tests for spinner component

--- src/components/atoms/spinner/index.test.tsx
+++ src/components/atoms/spinner/index.test.tsx
@@ -1,11 +1,51 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import Spinner from '.';
 
 describe('Spinner tests', () => {
-  const wrapper = shallow(<Spinner />);
+  const wrap = (props: { size?: string }) => mount(<Spinner {...props} />);
 
   it('should render the component', () => {
-    expect(wrapper).toMatchSnapshot();
+    expect(wrap({})).toMatchSnapshot();
+  });
+
+  it('should render a xsmall spinner', () => {
+    const size = 'xsmall';
+    const wrapper = wrap({ size });
+    expect(wrapper.props().size).toEqual(size);
+    expect(wrapper.find(`.${size}`)).toHaveLength(1);
+    wrapper.unmount();
+  });
+
+  it('should render a xsmall spinner', () => {
+    const size = 'small';
+    const wrapper = wrap({ size });
+    expect(wrapper.props().size).toEqual(size);
+    expect(wrapper.find(`.${size}`)).toHaveLength(1);
+    wrapper.unmount();
+  });
+
+  it('should render a medium spinner', () => {
+    const size = 'medium';
+    const wrapper = wrap({ size });
+    expect(wrapper.props().size).toEqual(size);
+    expect(wrapper.find(`.${size}`)).toHaveLength(1);
+    wrapper.unmount();
+  });
+
+  it('should render a large spinner', () => {
+    const size = 'large';
+    const wrapper = wrap({ size });
+    expect(wrapper.props().size).toEqual(size);
+    expect(wrapper.find(`.${size}`)).toHaveLength(1);
+    wrapper.unmount();
+  });
+
+  it('should render a xlarge spinner', () => {
+    const size = 'xlarge';
+    const wrapper = wrap({ size });
+    expect(wrapper.props().size).toEqual(size);
+    expect(wrapper.find(`.${size}`)).toHaveLength(1);
+    wrapper.unmount();
   });
 });
