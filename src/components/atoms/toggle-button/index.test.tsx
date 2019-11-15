2dab7558d74d3802ee71a669986d18674b44a9c7
Ivo Almeida <hyperlegend@hotmail.com>
2019-11-15 17:39:09 +0000

    Added unit tests to toggle button component

--- src/components/atoms/toggle-button/index.test.tsx
+++ src/components/atoms/toggle-button/index.test.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
-import ToggleButton from '.';
+import { mount } from 'enzyme';
+import ToggleButton, { IRadioItem, IToggleButtonProps } from '.';
 
 describe('ToggleButton tests', () => {
-  const wrapper = shallow(
-    <ToggleButton items={[{ label: 'Active', value: 1 }, { label: 'Inactive', value: 0 }]} />,
-  );
-
+  const wrap = (props: IToggleButtonProps) => mount(<ToggleButton {...props} />);
+  const items: IRadioItem[] = [{ label: 'Active', value: 1 }, { label: 'Inactive', value: 0 }];
   it('should render the component', () => {
-    expect(wrapper).toMatchSnapshot();
+    expect(wrap({ items })).toMatchSnapshot();
+  });
+
+  it('should render item props', () => {
+    const wrapper = wrap({ items });
+    expect(wrapper.props().items).toEqual(items);
+    expect(wrapper.find('input')).toHaveLength(2);
+    wrapper.unmount();
   });
 });
