0eec2751fff8929d00e60c3d6b2f7c4548f2c66e
Ivo Almeida <hyperlegend@hotmail.com>
2019-11-28 01:25:50 +0000

    Added unit tests

--- src/components/atoms/checkbox/index.stories.tsx
+++ src/components/atoms/checkbox/index.stories.tsx
@@ -1,8 +1,37 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
+import { actions } from '@storybook/addon-actions';
 
 import Checkbox from '.';
+import Label from '../label';
 
 storiesOf('Checkbox', module)
   .add('Checkbox default', () => <Checkbox />)
-  .add('Checkbox checked', () => <Checkbox checked />);
+  .add('Checkbox checked', () => <Checkbox checked />)
+  .add('Checkbox list', () => {
+    // let selected = {};
+    const [selected, setSelected] = React.useState({});
+    const transactions = [
+      { id: 'a1', label: 'City of Angels' },
+      { id: 'a2', label: 'When Harry met Sally' },
+      { id: 'a3', label: 'In the Cut' },
+    ];
+
+    const onChange = ({ target: { id, checked } }) => {
+      setSelected({ [id]: checked });
+      console.log(id, { [id]: checked });
+      console.log(id, selected[id]);
+    };
+    return (
+      <>
+        {transactions.map((t, i) => (
+          <React.Fragment key={`${t.id}_${i}`}>
+            <Checkbox id={t.id} checked={selected[t.id] || false} onChange={onChange} />
+            <Label htmlFor={t.id}>
+              {t.label} - {selected[t.id]}
+            </Label>
+          </React.Fragment>
+        ))}
+      </>
+    );
+  });
